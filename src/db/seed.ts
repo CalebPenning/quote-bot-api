2217f9d52af352b819ea84bf3315ccf2244c6df1
Caleb Penning <calebpenning@mail.com>
2023-03-19 15:27:02 -0700

    change to single quotes to see if the seed script runs like it's supposed to

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,6 +1,6 @@
 import sql from "./db"
-    ; (async () => {
-    console.log(sql)
+;(async () => {
+	console.log(sql)
 	await createTables()
 	await seedTables()
 	console.log("Tables created and seeded successfully")
@@ -40,44 +40,47 @@ async function createTables() {
 async function seedTables() {
 	try {
 		const quoteResults = await sql`
-            INSERT INTO quotes (body)
-                VALUES (
-                    "Start the war... that's never been seen before...",
-                    "EH HEH",
-                    "*holds knife to throat*",
-                    "*licks knife*",
-                    "*threatens to pour 'The Concoction' on you*",
-                    "Scientists studied it, dude",
-                    'My brother totally invented "Your face"',
-                    "Bro... Jane is so hot.",
-                    "That's some rope ass chicken",
-                    "Be right back, I gotta call my mom.\n*Hey, mom... I just wanted to call and say that I love you...*\nI'm back. *wipes tears*",
-                    "What if you made the connection... when there weren't any distractions?",
-                    "Look in the window...",
-                    "I'm the guest.",
-                    "©Joker1 Productions",
-                    "Ohhhh, what would you do with a drunken sailooor?",
-                    "I'm the best, man, I deed it",
-                    "Can someone turn the lights up? I can't hear the song.",
-                    "skating",
-                    "*holds knife to throat* DON'T YOU *EVER* TOUCH ME AGAIN",
-                    "I can see the clouds moving...",
-                    "I could model for apple sauce",
-                    "Well ya should've ate it!"
+            insert into quotes (
+                body
+            ) values (
+                    'Start the war... that's never been seen before...',
+                    'EH HEH',
+                    '*holds knife to throat*',
+                    '*licks knife*',
+                    '*threatens to pour 'The Concoction' on you*',
+                    'Scientists studied it, dude',
+                    'My brother totally invented 'Your face'',
+                    'Bro... Jane is so hot.',
+                    'That's some rope ass chicken',
+                    'Be right back, I gotta call my mom.\n*Hey, mom... I just wanted to call and say that I love you...*\nI'm back. *wipes tears*',
+                    'What if you made the connection... when there weren't any distractions?',
+                    'Look in the window...',
+                    'I'm the guest.',
+                    '©Joker1 Productions',
+                    'Ohhhh, what would you do with a drunken sailooor?',
+                    'I'm the best, man, I deed it',
+                    'Can someone turn the lights up? I can't hear the song.',
+                    'skating',
+                    '*holds knife to throat* DON'T YOU *EVER* TOUCH ME AGAIN',
+                    'I can see the clouds moving...',
+                    'I could model for apple sauce',
+                    'Well ya should've ate it!'
                 );
+            returning *
         `
 
 		const keywordsResults = await sql`
-            INSERT INTO keywords (body)
-                VALUES (
-                    "alex",
-                    "steven",
-                    "sneaky",
-                    "craigo",
-                    "stavi",
-                    "my brother",
-                    "epic"
+            insert into keywords (body)
+                values (
+                    'alex',
+                    'steven',
+                    'sneaky',
+                    'craigo',
+                    'stavi',
+                    'my brother',
+                    'epic'
                 );
+            returning *
         `
 
 		if (quoteResults && keywordsResults) {
