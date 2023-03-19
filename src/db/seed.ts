03781b6bbe0f6df227be81cd2b3aac57d172eeec
Caleb Penning <calebpenning@mail.com>
2023-03-19 16:07:29 -0700

    refactor seeding approach

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,4 +1,13 @@
 import sql from "./db"
+import { quotesSeed, keywordsSeed } from "./data"
+
+const mappedQuotes = quotesSeed.map((quote) => ({
+	body: quote,
+}))
+const mappedKeywords = keywordsSeed.map((keyword) => ({
+	body: keyword,
+}))
+
 ;(async () => {
 	console.log(sql)
 	await createTables()
@@ -41,46 +50,12 @@ async function createTables() {
 async function seedTables() {
 	try {
 		const quoteResults = await sql`
-            insert into quotes (
-                body
-            ) values (
-                    'Start the war... that\'s never been seen before...',
-                    'EH HEH',
-                    '*holds knife to throat*',
-                    '*licks knife*',
-                    '*threatens to pour \'The Concoction\' on you*',
-                    'Scientists studied it, dude',
-                    'My brother totally invented \'Your face\'',
-                    'Bro... Jane is so hot.',
-                    'That\'s some rope ass chicken',
-                    'Be right back, I gotta call my mom.\n*Hey, mom... I just wanted to call and say that I love you...*\nI\'\m back. *wipes tears*',
-                    'What if you made the connection... when there weren\'t any distractions?',
-                    'Look in the window...',
-                    'I\'m the guest.',
-                    '©Joker1 Productions',
-                    'Ohhhh, what would you do with a drunken sailooor?',
-                    'I\'m the best, man, I deed it',
-                    'Can someone turn the lights up? I can\'t hear the song.',
-                    'skating',
-                    '*holds knife to throat* DON\'T YOU *EVER* TOUCH ME AGAIN',
-                    'I can see the clouds moving...',
-                    'I could model for apple sauce',
-                    'Well ya should\'ve ate it!'
-                );
+            insert into quotes ${sql(mappedQuotes, "body")}
             returning *
         `
 
 		const keywordsResults = await sql`
-            insert into keywords (body)
-                values (
-                    'alex',
-                    'steven',
-                    'sneaky',
-                    'craigo',
-                    'stavi',
-                    'my brother',
-                    'epic'
-                );
+            insert into keywords ${sql(mappedKeywords, "body")}
             returning *
         `
 
