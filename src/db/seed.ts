a007ed0cd64d5e7dfbfeb7a960b71d82f68e9c45
Caleb Penning <calebpenning@mail.com>
2023-03-22 09:54:45 -0700

    comment out seed.ts until i figure out what to do with it

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,72 +1,74 @@
-import sql from "./db"
-import { quotesSeed, keywordsSeed } from "./data"
+// import sql from "./db"
+// import { quotesSeed, keywordsSeed } from "./data"
 
-// This file only exists to be executed if a brand new database has no entries,
-// we will not drop and reseed the tables with every deployment
+// // This file only exists to be executed if a brand new database has no entries,
+// // we will not drop and reseed the tables with every deployment
 
-const mappedQuotes = quotesSeed.map((quote) => ({
-	body: quote,
-}))
-const mappedKeywords = keywordsSeed.map((keyword) => ({
-	body: keyword,
-}))
+// // Both quotesSeed and keywordsSeed are arrays of strings, and in the db their id is serialized.
 
-;(async () => {
-	await createTables()
-	await seedTables()
-	console.log("Tables created and seeded successfully")
-})()
+// const mappedQuotes = quotesSeed.map((quote) => ({
+// 	body: quote,
+// }))
+// const mappedKeywords = keywordsSeed.map((keyword) => ({
+// 	body: keyword,
+// }))
 
-// create tables
-async function createTables() {
-	try {
-		const quotesResults = await sql`
-            CREATE TABLE IF NOT EXISTS quotes (
-                id SERIAL UNIQUE PRIMARY KEY,
-                body TEXT NOT NULL UNIQUE
-            )
-        `
-		if (quotesResults) {
-			console.log("Results of creating tables -> ", quotesResults)
-		}
+// ;(async () => {
+// 	await createTables()
+// 	await seedTables()
+// 	console.log("Tables created and seeded successfully")
+// })()
 
-		const keywordsResults = await sql`
-            CREATE TABLE IF NOT EXISTS keywords (
-                id SERIAL UNIQUE PRIMARY KEY,
-                body TEXT NOT NULL UNIQUE
-            )
-        `
+// // create tables
+// async function createTables() {
+// 	try {
+// 		const quotesResults = await sql`
+//             CREATE TABLE IF NOT EXISTS quotes (
+//                 id SERIAL UNIQUE PRIMARY KEY,
+//                 body TEXT NOT NULL UNIQUE
+//             )
+//         `
+// 		if (quotesResults) {
+// 			console.log("Results of creating tables -> ", quotesResults)
+// 		}
 
-		if (keywordsResults) {
-			console.log("Results of creating tables -> ", quotesResults)
-		}
+// 		const keywordsResults = await sql`
+//             CREATE TABLE IF NOT EXISTS keywords (
+//                 id SERIAL UNIQUE PRIMARY KEY,
+//                 body TEXT NOT NULL UNIQUE
+//             )
+//         `
 
-		console.log("Tables created successfully!")
-		return
-	} catch (err) {
-		console.error("An error occured while creating the tables: ")
-		console.error(err)
-	}
-}
+// 		if (keywordsResults) {
+// 			console.log("Results of creating tables -> ", quotesResults)
+// 		}
 
-async function seedTables() {
-	try {
-		const quoteResults = await sql`
-            insert into quotes ${sql(mappedQuotes, "body")}
-            returning *
-        `
+// 		console.log("Tables created successfully!")
+// 		return
+// 	} catch (err) {
+// 		console.error("An error occured while creating the tables: ")
+// 		console.error(err)
+// 	}
+// }
 
-		const keywordsResults = await sql`
-            insert into keywords ${sql(mappedKeywords, "body")}
-            returning *
-        `
+// async function seedTables() {
+// 	try {
+// 		const quoteResults = await sql`
+//             insert into quotes ${sql(mappedQuotes, "body")}
+//             returning *
+//         `
 
-		if (quoteResults && keywordsResults) {
-			console.log("Quote Insert Results -> ", quoteResults)
-			console.log("Keyword Insert Results -> ", keywordsResults)
-		}
-	} catch (err) {
-		console.error("An error occured while seeding the db tables: ")
-		console.error(err)
-	}
-}
+// 		const keywordsResults = await sql`
+//             insert into keywords ${sql(mappedKeywords, "body")}
+//             returning *
+//         `
+
+// 		if (quoteResults && keywordsResults) {
+// 			console.log("Quote Insert Results -> ", quoteResults)
+// 			console.log("Keyword Insert Results -> ", keywordsResults)
+// 		}
+// 	} catch (err) {
+// 		console.error("An error occured while seeding the db tables: ")
+// 		console.error(err)
+// 	}
+// }
